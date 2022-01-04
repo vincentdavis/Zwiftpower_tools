743af39282a23734febb00e17d2a0d327653b3ed
Vincent Davis <vincent@heteroskedastic.net>
2022-01-03 21:19:13 -0700

    work on wtrl reader

--- wtrldatareader.py
+++ wtrldatareader.py
@@ -1,14 +1,25 @@
-import requests
+from configparser import ConfigParser
 
-r = requests.post(url="https://www.wtrl.racing/api/wtrlruby.php",
-                  data={
-                      "wtrlid": "wtrlttt",
-                      "season": "140", # session
-                      "action": "results"
-                  },
-                  headers= {
-                      "Authorization": WTRL['bearer'], # static for ur account
-                      "Content-Type": "application/x-www-form-urlencoded"
-                  })
+import requests_html
+
+config = ConfigParser()
+config.read('config.ini')
+
+
+
+s = requests_html.session()
+r = s.get('https://www.wtrl.racing')
 print(r.status_code)
-print(r.json())
+# r = s.post(url="https://www.wtrl.racing/api/wtrlruby.php",
+#                   data={
+#                       "wtrlid": "wtrlttt",
+#                       "season": "140", # session
+#                       "action": "results"
+#                   },
+#                   headers= {
+#                       "Authorization": config['WTRL']['auth'], # static for ur account
+#                       "Content-Type": "application/x-www-form-urlencoded"
+#                   })
+# print(r.status_code)
+
+
