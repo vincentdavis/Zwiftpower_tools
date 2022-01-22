44c89eae82f5d826a5ad3de540d54bbf1c6b1eea
Vincent Davis <vincent@heteroskedastic.net>
2022-01-21 19:21:31 -0700

    strange rank error

--- races.py
+++ races.py
@@ -63,12 +63,14 @@ def rank_fts(df, sprints='All', group_col='category'):
     if sprints is 'All':
         sprints = [col for col in df.columns if 'msec_' in col]
     for sp in sprints:
-        ranks = df.groupby(group_col)[sp].rank(ascending = True, method='first')
+        ranks = df.groupby(group_col)[sp].rank(ascending=True, method='first')
         ranks.name = f"fts_{group_col}_{sp}_rank"
         df = pd.concat([df, ranks], axis=1)
         ranks_all = df.rank(ascending=True, method='first')
         ranks_all.name = f"fts_overall_{sp}_rank"
         df = pd.concat([df, ranks_all], axis=1)
+        ranks = None
+        ranks_all = None
     return df
 
 def melt_to_rows(df):
